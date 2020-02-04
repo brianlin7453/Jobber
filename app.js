98ce91ee4ae79ff88fd0380140d29536ff08c98c
Brian <brianlin7453@gmail.com>
2020-02-04 16:16:14 -0500

    Pushing project to github

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+//Index route
+app.get('/',(req, res) => {
+    const title = 'Welcome';
+    res.render('index', {
+        title: title
+    });
+});
+
+//About route
+app.get('/about',(req,res) => {
+    res.render('about');
+});
+
+const port = 5000;
+app.listen(port, ()=>{
+    console.log(`Server listening on port ${port}`);
+});
