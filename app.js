f85900584097a963ac0962b3177c50e079e30654
Brian <brianlin7453@gmail.com>
2020-02-04 16:31:43 -0500

    Connected to mongoDB

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
 
 const app = express();
 
+//Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+//Connect to mongoose
+mongoose
+  .connect("mongodb://localhost/quickjot-dev", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch(err => console.log(err));
 //Handlebars Middleware
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 //Index route
-app.get('/',(req, res) => {
-    const title = 'Welcome';
-    res.render('index', {
-        title: title
-    });
+app.get("/", (req, res) => {
+  const title = "Welcome";
+  res.render("index", {
+    title: title
+  });
 });
 
 //About route
-app.get('/about',(req,res) => {
-    res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 const port = 5000;
-app.listen(port, ()=>{
-    console.log(`Server listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
