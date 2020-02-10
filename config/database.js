b4d5e5d6f476a5041b0dc0553956920adaf995ee
Brian <brianlin7453@gmail.com>
2020-02-10 18:17:04 -0500

    Fixed db connection string

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://Brian:<password>@jobber-prod-tmspq.mongodb.net/test?retryWrites=true&w=majority'}
+    module.exports = {mongoURI: 'mongodb+srv://Brian:brian@jobber-prod-tmspq.mongodb.net/test?retryWrites=true&w=majority'}
 }
 else{
     module.exports = {
